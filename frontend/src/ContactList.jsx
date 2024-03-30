d36dc0f195eae59789a3123bc6e604dfd14d9621
Git-haan <ishaan.reddy5@gmail.com>
2024-03-30 12:40:24 -0400

    Update and Delete Functions

--- frontend/src/ContactList.jsx
+++ frontend/src/ContactList.jsx
@@ -1,6 +1,26 @@
 import React from "react";
 
 const ContactList = ({ contacts, updateContact, updateCallback }) => {
+
+    const onDelete = async (id) => {
+        try {
+            const options = {
+                method: 'DELETE',
+                headers: {
+                    'Content-Type': 'application/json',
+                }
+            }
+            const response = await fetch(`http://127.0.0.1:5000/delete_contact/${id}`, options)
+            if (response.status === 200) {
+                updateCallback()
+            } else {
+                alert('Failed to delete contact')
+            }
+        } catch (error) {
+            alert(error)
+        }
+    }
+
     return (
         <div>
             <h1>Contacts</h1>
@@ -8,8 +28,11 @@ const ContactList = ({ contacts, updateContact, updateCallback }) => {
                 <thead>
                     <tr>
                         <th>First Name</th>
+                        <th>{'  '}</th>
                         <th>Last Name</th>
+                        <th>{'  '}</th>
                         <th>Phone Number</th>
+                        <th>{'  '}</th>
                         <th>Actions</th>
                     </tr>
                 </thead>
@@ -17,11 +40,14 @@ const ContactList = ({ contacts, updateContact, updateCallback }) => {
                     {contacts.map((contact) => (
                         <tr key={contact.id}>
                             <td>{contact.firstName}</td>
+                            <td>{'  '}</td>
                             <td>{contact.lastName}</td>
+                            <td>{'  '}</td>
                             <td>{contact.phone}</td>
+                            <td>{'  '}</td>
                             <td>
-                                <button onClick = {() => updateCallback(contact)}>Update</button>
-                                <button>Delete</button>
+                                <button onClick = {() => updateContact(contact)}>Update</button>
+                                <button onClick = {() => onDelete(contact.id)}>Delete</button>
                             </td>
                         </tr>
                     ))}
